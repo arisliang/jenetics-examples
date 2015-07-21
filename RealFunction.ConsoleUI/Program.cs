237e9bdae4ac297e0810780f0b18a35f2d1f8b0a
Aris Liang <liangyi2012@gmail.com>
2015-07-22 00:14:33 +0800

    adds real function example.

--- RealFunction.ConsoleUI/Program.cs
+++ RealFunction.ConsoleUI/Program.cs
@@ -1,42 +1,48 @@
-﻿using System;
+﻿using org.jenetics;
+using org.jenetics.engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using org.jenetics;
-using org.jenetics.engine;
-using java.lang;
 
-namespace OnesCounting.ConsoleUI
+namespace RealFunction.ConsoleUI
 {
     class Program
     {
         /// <summary>
         /// Population size.
         /// </summary>
-        private static readonly int POPULATIONSIZE = 500;
+        private static readonly int POPULATION = 500;
 
         /// <summary>
         /// Mutation probability.
         /// </summary>
-        private static readonly double MUTATION = 0.55;
+        private static readonly double MUTATION = 0.03;
 
         /// <summary>
         /// Crossover probability.
+        /// https://github.com/jenetics/jenetics/blob/master/org.jenetics/src/main/java/org/jenetics/MeanAlterer.java
         /// </summary>
-        private static readonly double CROSSOVER = 0.06;
+        private static readonly double CROSSOVER = 0.6;
 
+        /// <summary>
+        /// http://jenetics.io/javadoc/org.jenetics/org/jenetics/engine/limit.html#bySteadyFitness-int-
+        /// </summary>
         private static readonly int STEADYLIMIT = 7;
 
+        /// <summary>
+        /// http://jenetics.io/javadoc/org.jenetics/org/jenetics/engine/EvolutionStream.html#limit-java.util.function.Predicate-
+        /// </summary>
         private static readonly int LIMIT = 100;
 
         static void Main(string[] args)
         {
             // Configure and build the evolution engine.
-            var engine = Engine.builder(new OnesCountingFunction(), BitChromosome.of(20, 0.15))
-                .populationSize(POPULATIONSIZE)
-                .selector(new RouletteWheelSelector())
-                .alterers(new Mutator(MUTATION), new SinglePointCrossover(CROSSOVER))
+            var engine = Engine.builder(new RealFunction(), DoubleChromosome.of(0, 2.0 * Math.PI))
+                .populationSize(POPULATION)
+                .optimize(Optimize.MINIMUM)
+                .alterers(new Mutator(MUTATION), new MeanAlterer(CROSSOVER))
                 .build();
 
             // Create evoluation statistics consumer
---
+++ RealFunction.ConsoleUI/RealFunction.cs
@@ -0,0 +1,47 @@
+﻿using java.util.function;
+using org.jenetics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealFunction.ConsoleUI
+{
+    public class RealFunction : Function
+    {
+        // This method calculates the fitness for a given genotype.
+        private static java.lang.Double eval(Genotype gt)
+        {
+            double x = ((DoubleGene)gt.getGene()).doubleValue();
+
+            double y = Math.Cos(0.5 + Math.Sin(x)) * Math.Cos(x);
+
+            return new java.lang.Double(y);
+        }
+
+        public Function andThen(Function after)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object apply(object t)
+        {
+            Genotype gt = (Genotype)t;
+
+            var y = eval(gt);
+
+            return y;
+        }
+
+        public Function compose(Function before)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Function identity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
